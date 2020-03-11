8d0b840c4830f890443e037f489362336fb60702
Aaron Kane <aaronkane@Aarons-MacBook-Pro.local>
2020-03-11 11:50:44 +1000

    second commit

--- domainscrape.rb
+++ domainscrape.rb
@@ -8,8 +8,9 @@ def scraper
   url   = 'https://www.domain.com.au/rent/zillmere-qld-4034/?price=0-600'
   unparsed_page = HTTParty.get(url)
   parsed_page    = Nokogiri::HTML(unparsed_page)
-  parsed_domain          = parsed_page.css('.listing-result__property-type').text
-  p parsed_domain
+  prop_type          = parsed_page.css('.listing-result__property-type').text
+  price          = parsed_page.css('.listing-result__price').text
+  byebug
 end
 scraper
 
